41b5306e1d00ce4f7060903af1191c1a9627a138
rutgerjohan <rutgerjohanp@gmail.com>
2026-08-24 10:26:17 +0200

    Update evaluation.js

--- evaluation.js
+++ evaluation.js
@@ -6,26 +6,6 @@ const pieceValues = {
 
 // Deze functie berekent de score van een positie door alle stukken op het bord te tellen en hun waarden op te tellen.
 // Bij 0,0 is de positie gelijk, - values betekent dat zwart beter is en + values betekent dat wit beter is.
-
-
-// function evaluateBoard(board) {
-//   let score = 0;
-
-//   for (let y = 0; y < 8; y++) {
-//     for (let x = 0; x < 8; x++) {
-//       let piece = board[y][x];
-//       if (pieceValues[piece]) {
-//         score += pieceValues[piece];
-//       }
-//     }
-//   }
-
-//   return score;
-//   console.log(score);
-// }
-
-
-
 function evaluateBoard(board) {
 
   const pieceValues = {
@@ -44,25 +24,9 @@ function evaluateBoard(board) {
       if (piece === ".") continue;
 
       score += pieceValues[piece] || 0;
-
-      // Center bonus
-      if (x >= 2 && x <= 5 && y >= 2 && y <= 5) {
-        score += isWhite(piece) ? 20 : -20;
-      }
-
-      // Pawn advancement bonus
-      if (piece === "P") {
-        score += (6 - y) * 10;
-      }
-      if (piece === "p") {
-        score -= (y - 1) * 10;
-      }
-
-      // Encourage knight + bishop development
-      if (piece === "N" && y < 7) score += 15;
-      if (piece === "n" && y > 0) score -= 15;
     }
   }
 
   return score;
+  console.log(score);
 }
